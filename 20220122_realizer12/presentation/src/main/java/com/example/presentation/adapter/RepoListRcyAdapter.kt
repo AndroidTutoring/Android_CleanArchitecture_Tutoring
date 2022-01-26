f081cc815417280d9016e925b7d7a081faafd237
leedonghun <dlwldlwldus123@gmail.com>
2022-01-26 23:08:27 +0900

    0주차 2번 과제 ->  상세 화면  구현

---
+++ 20220122_realizer12/presentation/src/main/java/com/example/presentation/adapter/RepoListRcyAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.example.presentation.databinding.ItemRepoInfoBinding
+import com.example.presentation.model.UserRepo
+import com.example.presentation.viewholder.RepoInfoViewHolder
+
+class RepoListRcyAdapter : ListAdapter<UserRepo, RepoInfoViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoInfoViewHolder {
+        val binding =
+            ItemRepoInfoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RepoInfoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RepoInfoViewHolder, position: Int) {
+        holder.apply {
+            bind(currentList[position])
+        }
+    }
+
+    override fun getItemCount() = currentList.size
+
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<UserRepo>() {
+            override fun areContentsTheSame(oldItem: UserRepo, newItem: UserRepo): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areItemsTheSame(oldItem: UserRepo, newItem: UserRepo): Boolean {
+                return oldItem.id == newItem.id
+            }
+        }
+    }
+}
--- 20220122_realizer12/presentation/src/main/java/com/example/presentation/retrofit/ApiService.kt
+++ 20220122_realizer12/presentation/src/main/java/com/example/presentation/retrofit/ApiService.kt
@@ -1,8 +1,10 @@
 package com.example.presentation.retrofit
 
 import com.example.presentation.model.SearchedUsers
+import com.example.presentation.model.UserRepo
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -13,5 +15,11 @@ interface ApiService {
         @Query("page") page:Int,
         @Query("per_page") perPage:Int
     ): Call<SearchedUsers>
+
+    //깃헙 유저 검색 하기
+    @GET("/users/{userName}/repos")
+    fun getUserRepoInfo(
+      @Path("userName") userName:String
+    ): Call<ArrayList<UserRepo>>
 
 }
