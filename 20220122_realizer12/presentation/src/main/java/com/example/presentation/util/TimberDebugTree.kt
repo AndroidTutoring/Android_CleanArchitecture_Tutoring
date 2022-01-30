eaa1b221dc57442934de5b8a9fd61d1b108fc325
leedonghun <dlwldlwldus123@gmail.com>
2022-01-30 20:10:32 +0900

    1주차 과제 -> 디버깅용  timber 라이브러리 추가

---
+++ 20220122_realizer12/presentation/src/main/java/com/example/presentation/util/TimberDebugTree.kt
@@ -0,0 +1,11 @@
+package com.example.presentation.util
+
+import timber.log.Timber
+
+class TimberDebugTree: Timber.DebugTree() {
+
+    //stack trace 내용 커스톰
+    override fun createStackElementTag(element: StackTraceElement): String {
+            return "(${element.fileName}:${element.lineNumber}) #${element.methodName}"
+    }
+}
