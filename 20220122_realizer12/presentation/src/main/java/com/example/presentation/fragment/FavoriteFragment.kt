7a84821f01db4db171c2293497b966d3768719a3
leedonghun <dlwldlwldus123@gmail.com>
2022-01-31 01:02:37 +0900

    1주차 과제 -> room 적용 및   유저 즐겨찾기 기능 추가

--- 20220122_realizer12/presentation/src/main/java/com/example/presentation/fragment/FavoriteFragment.kt
+++ 20220122_realizer12/presentation/src/main/java/com/example/presentation/fragment/FavoriteFragment.kt
@@ -2,13 +2,43 @@ package com.example.presentation.fragment
 
 import android.os.Bundle
 import android.view.View
+import com.example.presentation.adapter.UserListRcyAdapter
 import com.example.presentation.base.BaseFragment
 import com.example.presentation.databinding.FragmentFavoriteBinding
+import com.example.presentation.room.FavoriteMarkDataBase
+import timber.log.Timber
 
 //즐겨찾기 프래그먼트
 class FavoriteFragment: BaseFragment<FragmentFavoriteBinding>(FragmentFavoriteBinding::inflate) {
+
+    private lateinit var favoriteMarkedRcyAdapter: UserListRcyAdapter
+
+    private val favoriteMarkDataBase: FavoriteMarkDataBase? by lazy {
+        FavoriteMarkDataBase.getInstance(requireContext().applicationContext)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initSet()
+    }
 
+    override fun onResume() {
+        super.onResume()
+        getFavoriteGitUsers()
+    }
+
+    //초기세팅
+    private fun initSet(){
+
+        //리사이클러뷰 세팅
+       favoriteMarkedRcyAdapter = UserListRcyAdapter()
+       binding.rcyFavoriteList.apply {
+           adapter = favoriteMarkedRcyAdapter
+       }
+    }
+
+    //즐겨찾기한 유저들 가져옴.
+    private fun getFavoriteGitUsers(){
+        favoriteMarkedRcyAdapter.submitList(favoriteMarkDataBase?.getFavoriteMarkDao()?.getFavoriteGitUsers(true))
     }
 }
