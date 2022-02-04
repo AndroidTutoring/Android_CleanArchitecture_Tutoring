b1e6b50b387cec30563873befd2dc2de064e9ae3
DavidKwon7 <khjunejune10@gmail.com>
2022-02-04 16:13:28 +0900

    1주차 과제 : 객체 생성 위치 변경

--- 20220122_DavidKwon7/presentation/src/main/java/com/example/presentation/githubSource/remote/RemoteDataSourceImpl.kt
+++ 20220122_DavidKwon7/presentation/src/main/java/com/example/presentation/githubSource/remote/RemoteDataSourceImpl.kt
@@ -1,11 +1,13 @@
 package com.example.recylcerviewtest01.githubSource.remote
 
-import com.example.presentation.GithubRemoteRepoImpl
+import com.example.presentation.Api
 import com.example.presentation.User
 import io.reactivex.Single
-class RemoteDataSourceImpl :RemoteDataSource {
 
-    val api = GithubRemoteRepoImpl.service
+class RemoteDataSourceImpl(
+    private val api : Api
+) :RemoteDataSource {
+
 
     override fun getRepos(): Single<List<User>> {
         return api.getRepos("tkdgusl94")
