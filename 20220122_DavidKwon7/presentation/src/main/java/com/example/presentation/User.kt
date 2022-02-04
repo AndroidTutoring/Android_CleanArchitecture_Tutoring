e00e3abe8b2b01352890c04533cf12cb8387d6d5
DavidKwon7 <khjunejune10@gmail.com>
2022-02-04 12:46:48 +0900

    1주차 과제 : data class User에서 nullable 제거

--- 20220122_DavidKwon7/presentation/src/main/java/com/example/presentation/User.kt
+++ 20220122_DavidKwon7/presentation/src/main/java/com/example/presentation/User.kt
@@ -3,7 +3,7 @@ package com.example.presentation
 import androidx.room.Entity
 
 @Entity
-data class User(val name: String?,
-                val id: String?,
-                val date: String?,
+data class User(val name: String,
+                val id: String,
+                val date: String,
                 val url: String?)
--- 20220122_DavidKwon7/presentation/src/main/java/com/example/presentation/githubSource/remote/RemoteDataSourceImpl.kt
+++ 20220122_DavidKwon7/presentation/src/main/java/com/example/presentation/githubSource/remote/RemoteDataSourceImpl.kt
@@ -3,11 +3,9 @@ package com.example.recylcerviewtest01.githubSource.remote
 import com.example.presentation.GithubRemoteRepoImpl
 import com.example.presentation.User
 import io.reactivex.Single
-
 class RemoteDataSourceImpl :RemoteDataSource {
+
     val api = GithubRemoteRepoImpl.service
-
-
 
     override fun getRepos(): Single<List<User>> {
         return api.getRepos("tkdgusl94")
