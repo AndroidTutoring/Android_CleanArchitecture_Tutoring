1b5d84d238027242cb63f4fcfc59976518972a92
leedonghun <dlwldlwldus123@gmail.com>
2022-02-04 19:09:49 +0900

    1주차 과제 수정 ->  return 명시 되어있고 안되어있는 부분 통일

--- 20220122_realizer12/presentation/src/main/java/com/example/presentation/repository/UserRepositoryImpl.kt
+++ 20220122_realizer12/presentation/src/main/java/com/example/presentation/repository/UserRepositoryImpl.kt
@@ -1,37 +1,34 @@
 package com.example.presentation.repository
 
 import com.example.presentation.model.SearchedUser
-import com.example.presentation.model.SearchedUsers
 import com.example.presentation.source.local.UserLocalDataSource
-import com.example.presentation.source.local.UserLocalDataSourceImpl
 import com.example.presentation.source.remote.UserRemoteDataSource
 import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
-import retrofit2.Call
-import retrofit2.Callback
 
 class UserRepositoryImpl(
     private val userLocalDataSource: UserLocalDataSource,
     private val userRemoteDataSource: UserRemoteDataSource
 ):UserRepository {
 
-    override fun deleteFavoriteUser(deletedFavoriteUser: SearchedUser):Completable {
-        return userLocalDataSource.deleteFavoriteUser(deletedFavoriteUser)
-    }
+    override fun deleteFavoriteUser(deletedFavoriteUser: SearchedUser): Completable =
+        userLocalDataSource.deleteFavoriteUser(deletedFavoriteUser)
 
-    override fun addFavoriteUser(favoriteUser: SearchedUser):Completable {
-       return userLocalDataSource.addFavoriteUser(favoriteUser)
-    }
 
-    override fun getFavoriteUsers(): Single<List<SearchedUser>> = userLocalDataSource.getFavoriteUsers()
+    override fun addFavoriteUser(favoriteUser: SearchedUser): Completable =
+        userLocalDataSource.addFavoriteUser(favoriteUser)
+
+
+    override fun getFavoriteUsers(): Single<List<SearchedUser>> =
+        userLocalDataSource.getFavoriteUsers()
+
     override fun getSearchUsers(
         query: String,
         page: Int,
         perPage: Int
     ) = userRemoteDataSource.getSearchUsers(
-            query, page, perPage
-        )
+        query, page, perPage
+    )
 
 
 
