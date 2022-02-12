af16eaefa7f27223b719b257c2bf6b7a52e9f9a1
DavidKwon7 <khjunejune10@gmail.com>
2022-02-12 16:30:54 +0900

    data binding : build 추가

--- 20220122_DavidKwon7/data/src/main/java/com/example/data/repository/githubSource/remote/RemoteDataSource.kt
+++ 20220122_DavidKwon7/data/src/main/java/com/example/data/repository/githubSource/remote/RemoteDataSource.kt
@@ -1,6 +1,6 @@
 package com.example.data.repository.githubSource.remote
 
-import com.example.presentation.User
+import com.example.data.model.User
 import io.reactivex.Completable
 import io.reactivex.Single
 
