34b0f56d4fae754263bf0ecc33e6f6ec888df17a
DavidKwon7 <khjunejune10@gmail.com>
2022-02-14 19:48:11 +0900

    data mapping

--- 20220122_DavidKwon7/data/src/main/java/com/example/data/model/UserDataModel.kt
+++ 20220122_DavidKwon7/data/src/main/java/com/example/data/model/UserDataModel.kt
@@ -3,7 +3,6 @@ package com.example.data.model
 import androidx.room.Entity
 
 @Entity
-
 data class UserDataModel(val name: String,
                          val id: String,
                          val date: String,
