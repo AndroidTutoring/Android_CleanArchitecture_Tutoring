51094b85dade9b7b2165dc1abb60a874fb3f2dd5
leedonghun <dlwldlwldus123@gmail.com>
2022-02-16 01:59:01 +0900

    3주차 과제 -> RepoInfoViewholder databinding 적용

--- 20220122_realizer12/presentation/src/main/java/com/example/presentation/viewholder/RepoInfoViewHolder.kt
+++ 20220122_realizer12/presentation/src/main/java/com/example/presentation/viewholder/RepoInfoViewHolder.kt
@@ -7,16 +7,9 @@ import com.example.presentation.model.PresentationUserRepo
 class RepoInfoViewHolder(
     val binding: ItemRepoInfoBinding
 ) : RecyclerView.ViewHolder(binding.root) {
-
     fun bind(userRepo: PresentationUserRepo) {
 
-        //레포 이름 넣어줌.
-        binding.tvRepoName.text = userRepo.full_name
-
-        //레포 url 넣어줌.
-        binding.tvRepoUrl.text = userRepo.url
-
-        //레포 별점 넣어줌.
-        binding.tvStarCount.text = userRepo.stargazers_count.toString()
+        //데바용 repo 모델 연결
+        binding.userRepoModel = userRepo
     }
 }
