d2828f1c5fbb4e0aea723773a865a62a4b5623c3
leedonghun <dlwldlwldus123@gmail.com>
2022-02-19 02:26:16 +0900

    3주차 과제 수정 -> detailviewmodel -> 필요 없는 주석 제거

--- 20220122_realizer12/presentation/src/main/java/com/example/presentation/viewmodel/DetailViewModel.kt
+++ 20220122_realizer12/presentation/src/main/java/com/example/presentation/viewmodel/DetailViewModel.kt
@@ -14,11 +14,11 @@ class DetailViewModel(
     private val repoRepository: RepoRepository
 ) : BaseViewModel() {
 
-    //레포리스트 라이브데이터
+
     private val _repoDetailList = MutableLiveData<List<PresentationUserRepo>>()
     val repoDetailList: LiveData<List<PresentationUserRepo>> = _repoDetailList
 
-    //error 관련 처리용  라이브 데이터
+
     private val _error = MutableLiveData<Throwable>()
     val error: LiveData<Throwable> = _error
 
