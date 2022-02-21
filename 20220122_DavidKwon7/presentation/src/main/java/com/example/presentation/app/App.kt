e0b871d3d8b72621d0abf4a207f0bb1dd17f08f3
DavidKwon7 <khjunejune10@gmail.com>
2022-02-21 16:04:46 +0900

    hilt setting

--- 20220122_DavidKwon7/presentation/src/main/java/com/example/presentation/app/App.kt
+++ 20220122_DavidKwon7/presentation/src/main/java/com/example/presentation/app/App.kt
@@ -2,8 +2,10 @@ package com.example.presentation.app
 
 import android.app.Application
 import com.example.presentation.R
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
