fff00a46fd1d7dd4d4b874db3fe7ef308183df8a
leedonghun <dlwldlwldus123@gmail.com>
2022-02-23 23:53:44 +0900

    4주차 과제 ->  hilt  gradle 추가 및  application class에 annotation추가

--- 20220122_realizer12/app/src/main/java/com/example/cleanarchitecturetutoring/application/Application.kt
+++ 20220122_realizer12/app/src/main/java/com/example/cleanarchitecturetutoring/application/Application.kt
@@ -1,9 +1,10 @@
 package com.example.cleanarchitecturetutoring.application
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
-
+@HiltAndroidApp
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
