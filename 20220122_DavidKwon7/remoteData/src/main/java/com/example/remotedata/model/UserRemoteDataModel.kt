88c40904d5c2c2d653bcf71efeb741d68e399b7a
DavidKwon7 <khjunejune10@gmail.com>
2022-02-24 10:28:11 +0900

    remote module : mapper, model 연결

--- 20220122_DavidKwon7/remoteData/src/main/java/com/example/remotedata/model/UserRemoteDataModel.kt
+++ 20220122_DavidKwon7/remoteData/src/main/java/com/example/remotedata/model/UserRemoteDataModel.kt
@@ -1,8 +1,22 @@
 package com.example.remotedata.model
 
+import com.example.data.model.UserDataModel
+import com.example.remotedata.mapping.RemoteToUserMapper
+
 data class UserRemoteDataModel(
     val name: String,
     val id: String,
     val date: String,
     val url: String?
-)
+) {
+    companion object : RemoteToUserMapper<UserRemoteDataModel, UserDataModel> {
+        override fun toUser(userRemoteDataModel: UserRemoteDataModel): UserDataModel =
+            UserDataModel (
+                name = "",
+                id = "",
+                url = "",
+                date = ""
+            )
+
+    }
+}
