bccdd6f942bf1eadd6a91ff4efa0d66c3e8a8175
DavidKwon7 <khjunejune10@gmail.com>
2022-02-24 11:06:28 +0900

    localModule : mapper 연결

--- 20220122_DavidKwon7/localData/src/main/java/com/example/localdata/model/UserLocalDataModel.kt
+++ 20220122_DavidKwon7/localData/src/main/java/com/example/localdata/model/UserLocalDataModel.kt
@@ -1,9 +1,23 @@
 package com.example.localdata.model
 
+import com.example.data.model.UserDataModel
+import com.example.localdata.mapping.LocalToUserMapper
+
 data class UserLocalDataModel(
     val name: String,
     val id: String,
     val date: String,
     val url: String?
+) {
+    companion object : LocalToUserMapper<UserLocalDataModel, UserDataModel> {
+        override fun toUser(UserLocalDataModel: UserLocalDataModel): UserDataModel =
+            UserDataModel(
+                name = "",
+                id = "",
+                url = "",
+                date = ""
+            )
+    }
+}
 
-)
+
