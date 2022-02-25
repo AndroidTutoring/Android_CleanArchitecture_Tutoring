bc0bca6b9df8c9d4d6a4255871e49cb75fc8964e
DavidKwon7 <khjunejune10@gmail.com>
2022-02-25 10:45:46 +0900

    @Database 추가!

--- 20220122_DavidKwon7/localData/src/main/java/com/example/localdata/room/RoomDatabase.kt
+++ 20220122_DavidKwon7/localData/src/main/java/com/example/localdata/room/RoomDatabase.kt
@@ -1,9 +1,10 @@
 package com.example.localdata.room
 
-import android.content.Context
-import androidx.room.Room
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.localdata.model.UserLocalDataModel
 
+@Database(entities = [UserLocalDataModel::class], version = 1)
 abstract class RoomDatabase : RoomDatabase() {
     abstract fun userDao() : UserDao
 
