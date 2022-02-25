feebf6224848ed47f1d3b732fb1cbe43e0858977
leedonghun <dlwldlwldus123@gmail.com>
2022-02-26 00:36:43 +0900

    4주차 과제 수정 -> SearchedUserLocal -> 주석  상단으로  이동

--- 20220122_realizer12/local/src/main/java/com/example/local/model/SearchedUserLocal.kt
+++ 20220122_realizer12/local/src/main/java/com/example/local/model/SearchedUserLocal.kt
@@ -8,7 +8,20 @@ import androidx.room.PrimaryKey
 import com.example.data.model.SearchedUserDataModel
 import kotlinx.parcelize.Parcelize
 
-//로컬 데이터 베이스 즐겨찾기 테이블 구성
+/**
+ * Create Date: 2022/02/26
+ *
+ * @author leeDongHun
+ * Description: 로컬 데이터 베이스 즐겨찾기 테이블 구성
+ *
+ * @see uid 데이터 고유식별값 -> auto increment
+ * @see id 유저 아이디
+ * @see avatar_url 유저 프로필 url
+ * @see login 유저 닉네임
+ * @see html_url 유저 repo url
+ * @see isMyFavorite 유저 repo url
+ * */
+
 @Entity(
     tableName = "favoriteMarkTable",
     indices = [Index(value = ["user_id"], unique = true)]
@@ -16,12 +29,12 @@ import kotlinx.parcelize.Parcelize
 //깃헙 유저 모델
 @Parcelize
 data class SearchedUserLocal(
-    @PrimaryKey(autoGenerate = true) var uid: Long,//데이터 고유식별값 -> auto increment
-    @ColumnInfo(name = "user_id") var id: Long = 0L,//유저 아이디
-    @ColumnInfo(name = "avatar_url") var avatar_url: String = "",//유저 프로필 url
-    @ColumnInfo(name = "user_nick_name") var login: String = "",//유저 닉네임
-    @ColumnInfo(name = "repo_url") var html_url: String = "",//유저 repo url
-    @ColumnInfo(name = "is_my_favorite") var isMyFavorite: Boolean = false//유저 repo url
+    @PrimaryKey(autoGenerate = true) var uid: Long,
+    @ColumnInfo(name = "user_id") var id: Long = 0L,
+    @ColumnInfo(name = "avatar_url") var avatar_url: String = "",
+    @ColumnInfo(name = "user_nick_name") var login: String = "",
+    @ColumnInfo(name = "repo_url") var html_url: String = "",
+    @ColumnInfo(name = "is_my_favorite") var isMyFavorite: Boolean = false
 ) : Parcelable {
     companion object {
 
